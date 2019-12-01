be3e90d7fb17fade1c349c7fb5e562e2b6203ab5
Gerard Sans <gsans@amazon.com>
2019-12-01 09:37:12 +0800

    Add graphql dependencies

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import Auth from '@aws-amplify/auth';
+import API from '@aws-amplify/api';
+import PubSub from '@aws-amplify/pubsub';
 import amplify from './aws-exports';
 Auth.configure(amplify);
+API.configure(amplify);
+PubSub.configure(amplify);
 
 if (environment.production) {
   enableProdMode();
